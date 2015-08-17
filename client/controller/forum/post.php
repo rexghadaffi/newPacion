a26c88ea1a3417c222cf554c0280be807351e67f
rejunde <jhunfunclara.rudy@gmail.com>
2015-08-17 15:39:17 +0800

    jun
    sundot lang

---
+++ client/controller/forum/post.php
@@ -0,0 +1,14 @@
+<?php
+class post extends ForumViewClass
+{
+	function display()
+	{
+		/*
+		include_once "../data-access/mysql-db.php";
+		$obj=new DataAccessLayer;
+		$this->assign("topic", $obj->selectById("tbltopic", "topicID", $_GET["topicid"]));
+		$this->assign("posts", $obj->select("tblpost"));
+		*/
+	}      
+}
+?>
