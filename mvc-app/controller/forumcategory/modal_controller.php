05327a3dbfbaf6fb08cccd0f4de3576ef3955242
rejunde <jhunfunclara.rudy@gmail.com>
2015-08-24 16:20:52 +0800

    jun
    ./.

--- mvc-app/controller/forumcategory/modal_controller.php
+++ mvc-app/controller/forumcategory/modal_controller.php
@@ -14,5 +14,21 @@
 				);		
 				$query = new post;
 				$query->create("tblpost", $param);
-		}		
+		}
+
+
+	include_once "topic.php";
+	if (isset($_POST['btntopic']))
+		{	
+		$param = array(
+				"topicID" => "null",
+				"topicTitle" => $_POST['title'],
+				"topicDesc" => $_POST['desc'],
+				"dateCreated" => "",
+				"forumCatID" => $_GET["catid"],
+				"topicStatus" => 1,
+				);		
+				$query = new post;
+				$query->create("tbltopic", $param);
+		}
 ?>
